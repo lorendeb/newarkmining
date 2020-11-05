70092de5738fb9980c6bb7d6c3af1924a4921cf2
lorendery <lorendery@gmail.com>
2020-11-05 16:38:44 +0200

    corrected status bug

--- newark.py
+++ newark.py
@@ -9,75 +9,62 @@ page = requests.get(URL)
 
 # parse web data
 soup = BeautifulSoup(page.content, "html.parser")
-# print(soup.prettify())
 
+# create an empty list for flight destinations, then scrapped for all destinations (in order)
 destination_list = []
 for destination in soup.find_all('div', class_="flight-col flight-col__dest-term"):
     destination_list.append(destination.text.strip('\n\t'))
 
-hour_departure_list = []
-for hour_departure in soup.find_all('div', class_="flight-col flight-col__hour"):
-    hour_departure_list.append(hour_departure.text.strip('\n\t'))
-
+# create an empty list for flight numbers, then scrapped for all flight numbers (in order)
 flight_number_list = []
 for flight_number in soup.find_all('div', class_="flight-col flight-col__flight"):
     flight_number_list.append(flight_number.text.strip('\n\t'))
 
+# create an empty list for airlines, then scrapped for all airlines (in order)
 airline_list = []
-for flight_num in soup.find_all('div', class_="flight-col flight-col__dest-term"):
+for flight_num in soup.find_all('div', class_="flight-col flight-col__airline"):
     airline_list.append(flight_num.text.strip('\n\t'))
 
-terminal_departure_list = []
-for terminal_departure in soup.find_all('div', class_="flight-col flight-col__terminal-mob"):
-    terminal_departure_list.append(terminal_departure.text.strip('\n\t'))
-
-status_list = []
-for status in soup.find_all('div', class_="flight-col flight-col__status flight-col__status--GR"):
-    status_list.append(status.text.strip('\n\t'))
 
 detail_list = []
-
+status_list = []
+# for each flight number, get a new url to scrap more data
 for flight_num in flight_number_list:
     URL = "https://www.airport-ewr.com/newark-flight-departure/" + flight_num.splitlines()[0]
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     temp_list = []
+    # for each flight number, scrap for departure and arrival time, terminal and gate at both departure and arrival and status
     for detail in soup.find_all('div', class_="flight-info__infobox-text"):
         temp_list.append(detail.text.strip('\n\t'))
+    status = soup.find('div', class_="flight-status__title")
+    try:
+        status_list.append(status.text.strip('\n\t'))
+    except Exception:
+        status_list.append('Status')
     detail_list.append(temp_list)
 
+HOUR_DEPARTURE_INDEX = 0
+TERMINAL_DEPARTURE_INDEX = 1
 GATE_DEPARTURE_INDEX = 2
 HOUR_ARRIVAL_INDEX = 3
 TERMINAL_ARRIVAL_INDEX = 4
 GATE_ARRIVAL_INDEX = 5
 result_list = []
-for index in range(len(destination_list)):
+
+# create a finale list of lists of details per flight
+for index in range(1,len(destination_list)):
     temp = []
     temp.append(destination_list[index])
     temp.append(airline_list[index])
     temp.append(flight_number_list[index])
-    temp.append(hour_departure_list[index])
+    temp.append(detail_list[index][HOUR_DEPARTURE_INDEX])
     temp.append(detail_list[index][HOUR_ARRIVAL_INDEX])
-    temp.append(terminal_departure_list[index])
+    temp.append(detail_list[index][TERMINAL_DEPARTURE_INDEX])
     temp.append(detail_list[index][GATE_DEPARTURE_INDEX])
     temp.append(detail_list[index][TERMINAL_ARRIVAL_INDEX])
     temp.append(detail_list[index][GATE_ARRIVAL_INDEX])
     temp.append(status_list[index])
     result_list.append(temp)
 
-#
-#
-# print(detail_list)
-# print(destination_list)
-# print(hour_departure_list)
-# print(flight_number_list)
-# print(airline_list)
-# print(terminal_departure_list)
-# print(status_list)
-# print(len(destination_list))
-# print(len(hour_departure_list))
-# print(len(flight_number_list))
-# print(len(airline_list))
-# print(len(terminal_departure_list))
-# print(len(status_list))
-#
+print(result_list)
