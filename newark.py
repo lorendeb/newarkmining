283d779f94ce9789f6ca3466da0e1edf2a9aa15e
lorendery <lorendery@gmail.com>
2020-11-05 09:04:27 +0200

    updated name variables departure/arrival

--- newark.py
+++ newark.py
@@ -15,21 +15,21 @@ destination_list = []
 for destination in soup.find_all('div', class_="flight-col flight-col__dest-term"):
     destination_list.append(destination.text.strip('\n\t'))
 
-hour_list = []
-for hour in soup.find_all('div', class_="flight-col flight-col__hour"):
-    hour_list.append(hour.text.strip('\n\t'))
+hour_departure_list = []
+for hour_departure in soup.find_all('div', class_="flight-col flight-col__hour"):
+    hour_departure_list.append(hour_departure.text.strip('\n\t'))
 
-flight_list = []
-for flight in soup.find_all('div', class_="flight-col flight-col__flight"):
-    flight_list.append(flight.text.strip('\n\t'))
+flight_number_list = []
+for flight_number in soup.find_all('div', class_="flight-col flight-col__flight"):
+    flight_number_list.append(flight_number.text.strip('\n\t'))
 
 airline_list = []
 for flight_num in soup.find_all('div', class_="flight-col flight-col__dest-term"):
     airline_list.append(flight_num.text.strip('\n\t'))
 
-terminal_list = []
-for terminal in soup.find_all('div', class_="flight-col flight-col__terminal-mob"):
-    terminal_list.append(terminal.text.strip('\n\t'))
+terminal_departure_list = []
+for terminal_departure in soup.find_all('div', class_="flight-col flight-col__terminal-mob"):
+    terminal_departure_list.append(terminal_departure.text.strip('\n\t'))
 
 status_list = []
 for status in soup.find_all('div', class_="flight-col flight-col__status flight-col__status--GR"):
@@ -37,7 +37,7 @@ for status in soup.find_all('div', class_="flight-col flight-col__status flight-
 
 detail_list = []
 
-for flight_num in flight_list:
+for flight_num in flight_number_list:
     URL = "https://www.airport-ewr.com/newark-flight-departure/" + flight_num.splitlines()[0]
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -46,19 +46,20 @@ for flight_num in flight_list:
         temp_list.append(detail.text.strip('\n\t'))
     detail_list.append(temp_list)
 
+
+
+
 print(detail_list)
-
-
-# print(destination_list)
-# print(hour_list)
-print(flight_list)
+print(destination_list)
+print(hour_departure_list)
+print(flight_number_list)
 print(airline_list)
-# print(terminal_list)
-# print(status_list)
-# print(len(destination_list))
-# print(len(hour_list))
-# print(len(flight_list))
-# print(len(airline_list))
-# print(len(terminal_list))
-# print(len(status_list))
+print(terminal_departure_list)
+print(status_list)
+print(len(destination_list))
+print(len(hour_departure_list))
+print(len(flight_number_list))
+print(len(airline_list))
+print(len(terminal_departure_list))
+print(len(status_list))
 
